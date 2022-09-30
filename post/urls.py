5fa001ae2a12ac540f9f0a0a93df2c68fc545105
lucius-hyeon <lucius.hyeon@gmail.com>
2022-09-30 20:13:14 +0900

    post-modal add 사투중

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('test/', views.test_html, name='test'),
+    path('post/', views.post_add, name='post')
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,31 @@
+import re
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from post.models import ImageModel, PostModel
 from user.models import FollowModel
 # Create your views here.
 
 
-@login_required(login_url='user:login')
+def test_html(request):
+    # return render(request, 'post/temp_home.html')
+    return render(request, 'post/test.html')
+
+
+@login_required(login_url='login')
+def post_add(request):
+    if request.method == 'POST':
+        # author = request.POST.get('user_id', '')
+        user = request.user
+        content = request.POST.get('content', '')
+        image = request.POST.get('image', '')
+
+        my_post = PostModel.objects.create(author=user, content=content)
+        my_image = ImageModel.objects.create(post=my_post, image=image)
+
+        return render(request, 'index.html')
+
+
+@login_required(login_url='login')
 def index(request):
     user = request.user
     followers = FollowModel.objects.filter(user=user)
