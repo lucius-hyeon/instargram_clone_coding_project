0d23fc6672566e480652d0bb8d471af9893f03d6
lucius-hyeon <lucius.hyeon@gmail.com>
2022-10-04 09:42:26 +0900

    post 미완성 2차 commit

--- post/views.py
+++ post/views.py
@@ -1,10 +1,14 @@
+import os
+from uuid import uuid4
+from django.core.files.storage import FileSystemStorage  # 파일저장
+
 import re
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from instargram.settings import MEDIA_ROOT
 from post.models import ImageModel, PostModel
 from user.models import FollowModel, UserModel
 # Create your views here.
-
 
 
 def test_html(request):
@@ -15,13 +19,26 @@ def test_html(request):
 @login_required(login_url='login')
 def post_add(request):
     if request.method == 'POST':
-        # author = request.POST.get('user_id', '')
+
         user = request.user
         content = request.POST.get('content', '')
-        image = request.POST.get('image', '')
+        image = request.FILES.get('file')
+        image_name = request.POST.get('image', '')
+
+        # image_name = uuid4().hex
+        # save_path = os.path.join(MEDIA_ROOT, image_name)
+        # with open(save_path, 'wb+') as destination:
+        #     for chunk in image.Chunk():
+        #         destination.write(chunk)
+
+        # for image in request.FILES.getlist(key):
+        #     section = get_object_or_404(Section, pk = section_id[key])
+        #     ig = Image(section = section, image = image)
+        #     ig.save()
 
         my_post = PostModel.objects.create(author=user, content=content)
-        my_image = ImageModel.objects.create(post=my_post, image=image)
+        my_image = ImageModel.objects.create(
+            post=my_post, image=image, image_name=image_name)
 
         return render(request, 'index.html')
 
@@ -29,9 +46,13 @@ def post_add(request):
 @login_required(login_url='login')
 def index(request):
     user = request.user
+    post_list = PostModel.objects.all().order_by('-id')
+    image_list = ImageModel.objects.all().order_by('-post_id')
     followers = FollowModel.objects.filter(user=user)[:6]
     context = {
         'followers': followers,
+        'post_list': post_list,
+        'image_list': image_list,
         # 'storys' : get_sorted_story(user),
     }
     return render(request, 'index.html', context)
