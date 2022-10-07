656a911365809d81b4c99cd285570d8ec6bcc06d
Kim GyeongMin <gyeongmin951@naver.com>
2022-10-07 23:03:07 +0900

    Refactor : get_storys_author() 가독성 및 효율성 증대

--- story/views.py
+++ story/views.py
@@ -39,14 +39,18 @@ def view_story(request, nickname):
 # 로그인한 사용자의 팔로잉한 사람들의 스토리를 안 본 것과 본 것을 나눠보네는 함수
 
 
+# 로그인한 사용자의 팔로잉한 사람들의 스토리를 안 본 것과 본 것을 나눠보네는 함수
+
+
 def get_storys_author(request):
     user = request.user
     # 현재 로그인한 사람이 팔로잉한 사람들
-    followings = [f.follow for f in FollowModel.objects.filter(user=user)]
-    story_authors = []
+    followings = [f.follow for f in FollowModel.objects.filter(user=user).exclude(follow = user)]
+    new_story_authors = []
     viewed_story_authors = []
     for follow in followings:
-        instance_list = []
+        # 유효한 스토리(24시간이 지나지 않은 스토리)가 하나라도 있는지 판별할 변수이다.
+        is_valid = False
         # 팔로잉한 사람의 스토리를 한명씩 가져옴
         user_storys = Story.objects.filter(author=follow, is_end=False)
         for story in user_storys:
@@ -55,22 +59,20 @@ def get_storys_author(request):
                 story.is_end = True
                 story.save()
                 continue
-            # 유효하다면 인스턴스 리스트에 넣는다.
-            instance_list.append(story)
-        if len(instance_list) != 0:
-            # 로그인한 사용자가 본 스토리인지 아닌지 판별하여 넣어준다.
-            for story in instance_list:
-                try:
-                    StoryViewed.objects.get(story=story, user=user)
-                except StoryViewed.DoesNotExist:
-                    # 본 스토리가 아니라면 보지않은 스토리 작성자에 넣어준다.
-                    story_authors.append(story.author)
-                    break
-            else:
-                # 예외를 발생시키지 않고 모두 돌았다면 봤던 사용자로 넣어준다.
+            # 유효하다면 해당 스토리 작성자는 유효함을 남긴다.
+            is_valid = True
+            try:
+                StoryViewed.objects.get(story=story, user=user)
+            except StoryViewed.DoesNotExist:
+                # 본 스토리가 아니라면 보지않은 스토리 작성자에 넣어준다.
+                new_story_authors.append(story.author)
+                break
+        # 유효기간이 지난 스토리 혹은 다 봤던 스토리
+        else:
+            # 유효기간이 지나지 않은 스토리가 하나라도 있나
+            if is_valid:
                 viewed_story_authors.append(story.author)
-            # 61- instance_list에 넣고 for문을 돌리지 않고 바로 조건을 달아서 넣을 수 있어보인다.
-    return story_authors, viewed_story_authors
+    return new_story_authors, viewed_story_authors
 
 
 # 템플릿 태그 함수를 만드려는 함수(미완)
